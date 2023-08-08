6ff18c040d8ab6edf0d1d27896fea088fe843fe9
AZMCode <adrianozambrana@protonmail.com>
2023-08-08 16:43:13 -0400

    Updated script

--- sql/update-server-password.sh
+++ sql/update-server-password.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-printf "$(< ./04-server-auth.sql.template)" "$(< ./server_password)" > ./sql/04-server-auth.sql
+printf "$(< ./04-server-auth.sql.template)" "$(< ./server_password)" > ./dev/04-server-auth.sql
